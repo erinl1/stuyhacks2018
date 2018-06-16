812e30bd68a9c4db6306b3e683bb6a2b4085ebfe
erinl1 <erin.lee.ny@gmail.com>
2018-06-16 18:10:23 -0400

    Commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :metrocards
   resources :students
   resources :schools
+
+root to: 'students#splash'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
